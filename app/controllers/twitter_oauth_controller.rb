fa3a0fbb54db5913dd9f42ed4b3365625974f662
katie Pekarek <katie_pekarek@yahoo.com>
2015-06-29 15:01:28 -0600

    refactor routes

--- app/controllers/twitter_oauth_controller.rb
+++ app/controllers/twitter_oauth_controller.rb
@@ -4,7 +4,7 @@ class TwitterOauthController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_or_create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in with Twitter!"
+    redirect_to welcome_index_path, :notice => "Signed in with Twitter!"
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root 'bleacher_report#new'
   get '/sign-in' => 'bleacher_report#new', as: :signin
   post '/sign-in' => 'bleacher_report#create'
   get "/auth/:provider/callback" => "twitter_oauth#create"
   get "/signout" => "sessions#destroy", :as => :signout
 
+  resources :welcome
   resources :tweets
-
-  require 'sidekiq/web'
-  # ...
-  mount Sidekiq::Web, at: '/sidekiq'
 
 end
